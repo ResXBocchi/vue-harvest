a27dd835b390f370b11a9799c1b2c4f35ecf0efe
unknown <bocchi.dev@gmail.com>
2021-01-18 11:30:32 -0300

    changing to vue3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template >
+  <Storytelling />
+  <Form />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Storytelling from '../components/Storytelling.vue';
+import Form from '../components/Form.vue';
 export default {
-  name: "Home",
+  name: 'App',
   components: {
-    HelloWorld
+    Storytelling,
+    Form
   }
 };
 </script>
